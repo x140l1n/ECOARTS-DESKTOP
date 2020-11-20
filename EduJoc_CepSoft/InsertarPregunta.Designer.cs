d549082b14d6a57116475d5ccd1965d207fdf8d2
Oriol <urimipe@gmail.com>
2020-11-20 13:57:13 +0100

    continuacion diseño formulario insertar preguntas

--- EduJoc_CepSoft/InsertarPregunta.Designer.cs
+++ EduJoc_CepSoft/InsertarPregunta.Designer.cs
@@ -36,37 +36,29 @@
             this.textBoxPregunta = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxResp1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-<<<<<<< HEAD:EduJoc_CepSoft/InsertarPreguntas.Designer.cs
             this.label1.Location = new System.Drawing.Point(22, 19);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(58, 23);
-=======
-            this.label1.Location = new System.Drawing.Point(19, 19);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
->>>>>>> d3c7d80e715a5c8753ac24dcaec8b1c34fcc47bd:EduJoc_CepSoft/InsertarPregunta.Designer.cs
             this.label1.TabIndex = 0;
             this.label1.Text = "Tema: ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-<<<<<<< HEAD:EduJoc_CepSoft/InsertarPreguntas.Designer.cs
             this.label2.Location = new System.Drawing.Point(285, 19);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(64, 23);
-=======
-            this.label2.Location = new System.Drawing.Point(216, 19);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 13);
->>>>>>> d3c7d80e715a5c8753ac24dcaec8b1c34fcc47bd:EduJoc_CepSoft/InsertarPregunta.Designer.cs
             this.label2.TabIndex = 1;
             this.label2.Text = "Idioma: ";
             // 
@@ -74,64 +66,35 @@
             // 
             this.comboBoxTema.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxTema.FormattingEnabled = true;
-<<<<<<< HEAD:EduJoc_CepSoft/InsertarPreguntas.Designer.cs
             this.comboBoxTema.Location = new System.Drawing.Point(25, 46);
             this.comboBoxTema.Name = "comboBoxTema";
             this.comboBoxTema.Size = new System.Drawing.Size(240, 30);
-=======
-            this.comboBoxTema.Location = new System.Drawing.Point(21, 35);
-            this.comboBoxTema.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.comboBoxTema.Name = "comboBoxTema";
-            this.comboBoxTema.Size = new System.Drawing.Size(181, 21);
->>>>>>> d3c7d80e715a5c8753ac24dcaec8b1c34fcc47bd:EduJoc_CepSoft/InsertarPregunta.Designer.cs
             this.comboBoxTema.TabIndex = 2;
             // 
             // comboBoxIdioma
             // 
             this.comboBoxIdioma.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxIdioma.FormattingEnabled = true;
-<<<<<<< HEAD:EduJoc_CepSoft/InsertarPreguntas.Designer.cs
             this.comboBoxIdioma.Location = new System.Drawing.Point(288, 46);
             this.comboBoxIdioma.Name = "comboBoxIdioma";
             this.comboBoxIdioma.Size = new System.Drawing.Size(240, 30);
-=======
-            this.comboBoxIdioma.Location = new System.Drawing.Point(218, 35);
-            this.comboBoxIdioma.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.comboBoxIdioma.Name = "comboBoxIdioma";
-            this.comboBoxIdioma.Size = new System.Drawing.Size(181, 21);
->>>>>>> d3c7d80e715a5c8753ac24dcaec8b1c34fcc47bd:EduJoc_CepSoft/InsertarPregunta.Designer.cs
             this.comboBoxIdioma.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-<<<<<<< HEAD:EduJoc_CepSoft/InsertarPreguntas.Designer.cs
             this.label3.Location = new System.Drawing.Point(22, 91);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(73, 23);
-=======
-            this.label3.Location = new System.Drawing.Point(19, 62);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(50, 13);
->>>>>>> d3c7d80e715a5c8753ac24dcaec8b1c34fcc47bd:EduJoc_CepSoft/InsertarPregunta.Designer.cs
             this.label3.TabIndex = 4;
             this.label3.Text = "Pregunta";
             // 
             // textBoxPregunta
             // 
-<<<<<<< HEAD:EduJoc_CepSoft/InsertarPreguntas.Designer.cs
             this.textBoxPregunta.Location = new System.Drawing.Point(25, 119);
             this.textBoxPregunta.Multiline = true;
             this.textBoxPregunta.Name = "textBoxPregunta";
             this.textBoxPregunta.Size = new System.Drawing.Size(500, 161);
-=======
-            this.textBoxPregunta.Location = new System.Drawing.Point(21, 78);
-            this.textBoxPregunta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.textBoxPregunta.Multiline = true;
-            this.textBoxPregunta.Name = "textBoxPregunta";
-            this.textBoxPregunta.Size = new System.Drawing.Size(376, 97);
->>>>>>> d3c7d80e715a5c8753ac24dcaec8b1c34fcc47bd:EduJoc_CepSoft/InsertarPregunta.Designer.cs
             this.textBoxPregunta.TabIndex = 5;
             // 
             // label4
@@ -147,32 +110,79 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(17, 321);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(212, 20);
+            this.textBox1.Size = new System.Drawing.Size(212, 22);
             this.textBox1.TabIndex = 7;
             // 
-            // InsertarPreguntas
+            // label5
             // 
-<<<<<<< HEAD:EduJoc_CepSoft/InsertarPreguntas.Designer.cs
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 304);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 23);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Respuesta 1";
+            // 
+            // textBoxResp1
+            // 
+            this.textBoxResp1.Location = new System.Drawing.Point(26, 330);
+            this.textBoxResp1.Multiline = true;
+            this.textBoxResp1.Name = "textBoxResp1";
+            this.textBoxResp1.Size = new System.Drawing.Size(379, 87);
+            this.textBoxResp1.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 434);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 23);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Respuesta 2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(26, 460);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(380, 87);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 567);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 23);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Respuesta 3";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(26, 593);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(381, 87);
+            this.textBox3.TabIndex = 11;
+            // 
+            // InsertarPregunta
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(565, 866);
-=======
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(420, 512);
->>>>>>> d3c7d80e715a5c8753ac24dcaec8b1c34fcc47bd:EduJoc_CepSoft/InsertarPregunta.Designer.cs
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxResp1);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.textBoxPregunta);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBoxIdioma);
             this.Controls.Add(this.comboBoxTema);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-<<<<<<< HEAD:EduJoc_CepSoft/InsertarPreguntas.Designer.cs
             this.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-=======
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
->>>>>>> d3c7d80e715a5c8753ac24dcaec8b1c34fcc47bd:EduJoc_CepSoft/InsertarPregunta.Designer.cs
-            this.Name = "InsertarPreguntas";
+            this.Name = "InsertarPregunta";
             this.Text = "Insertar pregunta";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -189,5 +199,11 @@
         private System.Windows.Forms.TextBox textBoxPregunta;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxResp1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
