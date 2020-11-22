a35f9f2888f75fb53b1fc671a5ef9d15b8998888
Oriol <urimipe@gmail.com>
2020-11-22 13:18:30 +0100

    se añade formulario insertar pregunta y verRespuesta

--- EduJoc_CepSoft/InsertarPregunta.Designer.cs
+++ EduJoc_CepSoft/InsertarPregunta.Designer.cs
@@ -42,6 +42,13 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonResp1 = new System.Windows.Forms.RadioButton();
+            this.radioButtonResp2 = new System.Windows.Forms.RadioButton();
+            this.radioButtonResp3 = new System.Windows.Forms.RadioButton();
+            this.labelId = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -94,7 +101,7 @@
             this.textBoxPregunta.Location = new System.Drawing.Point(25, 119);
             this.textBoxPregunta.Multiline = true;
             this.textBoxPregunta.Name = "textBoxPregunta";
-            this.textBoxPregunta.Size = new System.Drawing.Size(500, 161);
+            this.textBoxPregunta.Size = new System.Drawing.Size(503, 161);
             this.textBoxPregunta.TabIndex = 5;
             // 
             // label4
@@ -164,11 +171,78 @@
             this.textBox3.Size = new System.Drawing.Size(381, 87);
             this.textBox3.TabIndex = 11;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonResp3);
+            this.groupBox1.Controls.Add(this.radioButtonResp2);
+            this.groupBox1.Controls.Add(this.radioButtonResp1);
+            this.groupBox1.Location = new System.Drawing.Point(413, 330);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(112, 350);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // radioButtonResp1
+            // 
+            this.radioButtonResp1.AutoSize = true;
+            this.radioButtonResp1.Checked = true;
+            this.radioButtonResp1.Location = new System.Drawing.Point(6, 41);
+            this.radioButtonResp1.Name = "radioButtonResp1";
+            this.radioButtonResp1.Size = new System.Drawing.Size(90, 27);
+            this.radioButtonResp1.TabIndex = 0;
+            this.radioButtonResp1.TabStop = true;
+            this.radioButtonResp1.Text = "Correcta";
+            this.radioButtonResp1.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonResp2
+            // 
+            this.radioButtonResp2.AutoSize = true;
+            this.radioButtonResp2.Location = new System.Drawing.Point(6, 165);
+            this.radioButtonResp2.Name = "radioButtonResp2";
+            this.radioButtonResp2.Size = new System.Drawing.Size(90, 27);
+            this.radioButtonResp2.TabIndex = 1;
+            this.radioButtonResp2.TabStop = true;
+            this.radioButtonResp2.Text = "Correcta";
+            this.radioButtonResp2.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonResp3
+            // 
+            this.radioButtonResp3.AutoSize = true;
+            this.radioButtonResp3.Location = new System.Drawing.Point(6, 295);
+            this.radioButtonResp3.Name = "radioButtonResp3";
+            this.radioButtonResp3.Size = new System.Drawing.Size(90, 27);
+            this.radioButtonResp3.TabIndex = 2;
+            this.radioButtonResp3.TabStop = true;
+            this.radioButtonResp3.Text = "Correcta";
+            this.radioButtonResp3.UseVisualStyleBackColor = true;
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Location = new System.Drawing.Point(43, 811);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(116, 23);
+            this.labelId.TabIndex = 13;
+            this.labelId.Text = "#IdModificacion";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(377, 746);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 47);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Insertar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // InsertarPregunta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(565, 866);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelId);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.textBox2);
@@ -184,6 +258,8 @@
             this.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "InsertarPregunta";
             this.Text = "Insertar pregunta";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -205,5 +281,11 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonResp3;
+        private System.Windows.Forms.RadioButton radioButtonResp2;
+        private System.Windows.Forms.RadioButton radioButtonResp1;
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.Button button1;
     }
 }
---
+++ EduJoc_CepSoft/VerRespuestas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EduJoc_CepSoft
+{
+    public partial class VerRespuestas : Form
+    {
+        public VerRespuestas()
+        {
+            InitializeComponent();
+        }
+    }
+}
