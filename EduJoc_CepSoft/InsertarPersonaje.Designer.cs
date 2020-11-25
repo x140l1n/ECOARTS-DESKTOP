a2d59708f487377fc78b45d3e424e1f410fe2adf
cep <cep@cepnet0.net>
2020-11-25 10:25:13 +0100

    conflictes

---
+++ EduJoc_CepSoft/InsertarPersonaje.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace EduJoc_CepSoft
+{
+    partial class InsertarPersonaje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InsertarPersonaje));
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtboxNombre = new System.Windows.Forms.TextBox();
+            this.lblImagen = new System.Windows.Forms.Label();
+            this.btnImagen = new System.Windows.Forms.Button();
+            this.picboxImagen = new System.Windows.Forms.PictureBox();
+            this.lblDescCatalan = new System.Windows.Forms.Label();
+            this.txtboxDescCatalan = new System.Windows.Forms.TextBox();
+            this.lblDescIngles = new System.Windows.Forms.Label();
+            this.lblDescCatsellano = new System.Windows.Forms.Label();
+            this.txtboxDescIngles = new System.Windows.Forms.TextBox();
+            this.txtboxDescCastellano = new System.Windows.Forms.TextBox();
+            this.btnAñadir = new System.Windows.Forms.Button();
+            this.labelId = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(13, 13);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 14);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtboxNombre
+            // 
+            this.txtboxNombre.Location = new System.Drawing.Point(12, 30);
+            this.txtboxNombre.Name = "txtboxNombre";
+            this.txtboxNombre.Size = new System.Drawing.Size(351, 20);
+            this.txtboxNombre.TabIndex = 1;
+            // 
+            // lblImagen
+            // 
+            this.lblImagen.AutoSize = true;
+            this.lblImagen.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImagen.Location = new System.Drawing.Point(13, 79);
+            this.lblImagen.Name = "lblImagen";
+            this.lblImagen.Size = new System.Drawing.Size(44, 14);
+            this.lblImagen.TabIndex = 2;
+            this.lblImagen.Text = "Imagen:";
+            // 
+            // btnImagen
+            // 
+            this.btnImagen.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnImagen.Location = new System.Drawing.Point(63, 75);
+            this.btnImagen.Name = "btnImagen";
+            this.btnImagen.Size = new System.Drawing.Size(40, 23);
+            this.btnImagen.TabIndex = 3;
+            this.btnImagen.Text = "...";
+            this.btnImagen.UseVisualStyleBackColor = true;
+            // 
+            // picboxImagen
+            // 
+            this.picboxImagen.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.picboxImagen.Location = new System.Drawing.Point(12, 106);
+            this.picboxImagen.Name = "picboxImagen";
+            this.picboxImagen.Size = new System.Drawing.Size(351, 294);
+            this.picboxImagen.TabIndex = 4;
+            this.picboxImagen.TabStop = false;
+            // 
+            // lblDescCatalan
+            // 
+            this.lblDescCatalan.AutoSize = true;
+            this.lblDescCatalan.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescCatalan.Location = new System.Drawing.Point(396, 14);
+            this.lblDescCatalan.Name = "lblDescCatalan";
+            this.lblDescCatalan.Size = new System.Drawing.Size(105, 14);
+            this.lblDescCatalan.TabIndex = 5;
+            this.lblDescCatalan.Text = "Descripción catalán:";
+            // 
+            // txtboxDescCatalan
+            // 
+            this.txtboxDescCatalan.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtboxDescCatalan.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxDescCatalan.Location = new System.Drawing.Point(399, 30);
+            this.txtboxDescCatalan.Multiline = true;
+            this.txtboxDescCatalan.Name = "txtboxDescCatalan";
+            this.txtboxDescCatalan.Size = new System.Drawing.Size(350, 97);
+            this.txtboxDescCatalan.TabIndex = 6;
+            // 
+            // lblDescIngles
+            // 
+            this.lblDescIngles.AutoSize = true;
+            this.lblDescIngles.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescIngles.Location = new System.Drawing.Point(399, 284);
+            this.lblDescIngles.Name = "lblDescIngles";
+            this.lblDescIngles.Size = new System.Drawing.Size(98, 14);
+            this.lblDescIngles.TabIndex = 12;
+            this.lblDescIngles.Text = "Descripción inglés:";
+            // 
+            // lblDescCatsellano
+            // 
+            this.lblDescCatsellano.AutoSize = true;
+            this.lblDescCatsellano.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescCatsellano.Location = new System.Drawing.Point(399, 146);
+            this.lblDescCatsellano.Name = "lblDescCatsellano";
+            this.lblDescCatsellano.Size = new System.Drawing.Size(119, 14);
+            this.lblDescCatsellano.TabIndex = 10;
+            this.lblDescCatsellano.Text = "Descripción castellano:";
+            // 
+            // txtboxDescIngles
+            // 
+            this.txtboxDescIngles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtboxDescIngles.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxDescIngles.Location = new System.Drawing.Point(399, 303);
+            this.txtboxDescIngles.Multiline = true;
+            this.txtboxDescIngles.Name = "txtboxDescIngles";
+            this.txtboxDescIngles.Size = new System.Drawing.Size(350, 97);
+            this.txtboxDescIngles.TabIndex = 13;
+            // 
+            // txtboxDescCastellano
+            // 
+            this.txtboxDescCastellano.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtboxDescCastellano.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtboxDescCastellano.Location = new System.Drawing.Point(399, 165);
+            this.txtboxDescCastellano.Multiline = true;
+            this.txtboxDescCastellano.Name = "txtboxDescCastellano";
+            this.txtboxDescCastellano.Size = new System.Drawing.Size(350, 97);
+            this.txtboxDescCastellano.TabIndex = 11;
+            // 
+            // btnAñadir
+            // 
+            this.btnAñadir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAñadir.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAñadir.Location = new System.Drawing.Point(654, 417);
+            this.btnAñadir.Name = "btnAñadir";
+            this.btnAñadir.Size = new System.Drawing.Size(95, 23);
+            this.btnAñadir.TabIndex = 14;
+            this.btnAñadir.Text = "Añadir";
+            this.btnAñadir.UseVisualStyleBackColor = true;
+            // 
+            // labelId
+            // 
+            this.labelId.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelId.AutoSize = true;
+            this.labelId.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelId.Location = new System.Drawing.Point(13, 421);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(81, 14);
+            this.labelId.TabIndex = 15;
+            this.labelId.Text = "#IdModificacion";
+            // 
+            // InsertarPersonaje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 452);
+            this.Controls.Add(this.labelId);
+            this.Controls.Add(this.btnAñadir);
+            this.Controls.Add(this.lblDescIngles);
+            this.Controls.Add(this.lblDescCatsellano);
+            this.Controls.Add(this.txtboxDescIngles);
+            this.Controls.Add(this.txtboxDescCastellano);
+            this.Controls.Add(this.txtboxDescCatalan);
+            this.Controls.Add(this.lblDescCatalan);
+            this.Controls.Add(this.picboxImagen);
+            this.Controls.Add(this.btnImagen);
+            this.Controls.Add(this.lblImagen);
+            this.Controls.Add(this.txtboxNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "InsertarPersonaje";
+            this.Text = "Insertar Personaje";
+            ((System.ComponentModel.ISupportInitialize)(this.picboxImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtboxNombre;
+        private System.Windows.Forms.Label lblImagen;
+        private System.Windows.Forms.Button btnImagen;
+        private System.Windows.Forms.PictureBox picboxImagen;
+        private System.Windows.Forms.Label lblDescCatalan;
+        private System.Windows.Forms.TextBox txtboxDescCatalan;
+        private System.Windows.Forms.Label lblDescIngles;
+        private System.Windows.Forms.Label lblDescCatsellano;
+        private System.Windows.Forms.TextBox txtboxDescIngles;
+        private System.Windows.Forms.TextBox txtboxDescCastellano;
+        private System.Windows.Forms.Button btnAñadir;
+        private System.Windows.Forms.Label labelId;
+    }
+}
